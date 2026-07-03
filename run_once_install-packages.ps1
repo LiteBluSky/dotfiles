ec5f130c126b5e03d04ff5a4b5216918d519b162
LiteBluSky <rbuckbamford@gmail.com>
2026-07-03 23:41:51 +0200

    "new file:   run_once_install-packages.ps1"

---
+++ run_once_install-packages.ps1
@@ -0,0 +1,17 @@
+# Write-Host outputs a status message during 'chezmoi apply'
+Write-Host "Checking core apps..."
+
+# Check if winget is available
+if (Get-Command winget -ErrorAction SilentlyContinue) {
+    
+    # Install Zen Browser if it isn't already installed
+    Write-Host "Installing Zen Browser via WinGet..."
+    winget install -e --id ZenBrowser.ZenBrowser --accept-source-agreements --accept-package-agreements
+
+    # You can stack other apps here too!
+    # winget install -e --id JanDeDobbeleer.OhMyPosh
+    # winget install -e --id Git.Git
+
+} else {
+    Write-Warning "WinGet was not found on this system. Skipping application installs."
+}
