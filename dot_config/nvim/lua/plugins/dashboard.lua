31f39afba28f63cecd4374780042f6126f82b45b
Ryan Kalfrisa <ryan.buck@kalfrisa.com>
2026-07-09 15:27:20 +0200

    lazyvim: updated dashboard config

--- dot_config/nvim/lua/plugins/dashboard.lua
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -3,7 +3,23 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
+        width = 16,
         preset = {
+          keys = {
+            { icon = "󰱼", key = "f", desc = "[F]ind file", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = "", key = "n", desc = "[N]ew file", action = ":ene | startinsert" },
+            { icon = "", key = "g", desc = "[G]rep text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = "󰒮", key = "r", desc = "[R]ecent file", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = "",
+              key = "c",
+              desc = "[C]onfig",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = "", key = "s", desc = "[S]ession", section = "session" },
+            { icon = "󰬇", key = "L", desc = "[L]azy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = "", key = "q", desc = "[Q]uit", action = ":qa" },
+          },
           header = [[
                                                                                    Z
                                                                                z
@@ -19,6 +35,9 @@ return {
                                   ░░██████                                
                                    ░░░░░░                                 
           ]],
+        },
+        formats = {
+          key = { "" },
         },
       },
     },
